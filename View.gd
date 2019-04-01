4bf742d563b804b4d72734995f996b523535e9e4
Unknown <mattias.myhrman@gmail.com>
2019-04-02 00:12:05 +0200

    added tabs

--- View.gd
+++ View.gd
@@ -3,9 +3,22 @@ extends ColorRect
 signal drag(event)
 signal release(view)
 
+func _ready():
+	_on_TabButton_pressed(0)
+	
 func _gui_input(event):
 	if event is InputEventScreenDrag: 
 		emit_signal("drag",event)
 
 	if event is InputEventScreenTouch and not event.is_pressed():
 		emit_signal("release",self)
+
+
+func _on_TabButton_pressed(tab_idx):
+	for tab in $tabs.get_children():
+		tab.tabButton.pressed = false
+		tab.panel.visible = false
+		
+	$tabs.get_child(tab_idx).panel.visible = true
+	$tabs.get_child(tab_idx).tabButton.pressed = true
+
---
+++ tab.gd
@@ -0,0 +1,4 @@
+extends Control
+
+onready var tabButton = get_node("VBoxContainer/HBoxContainer/TabButton")
+onready var panel = get_node("VBoxContainer/Panel")
