7f5020ca661ad3ba848d4982fd116042fa9d6c44
Unknown <mattias.myhrman@gmail.com>
2019-04-02 23:20:23 +0200

    last fix

--- Main.gd
+++ Main.gd
@@ -1,31 +1,22 @@
 extends Node
 
 var deadzone = 150
-
+var currentPosX = 0
 func _on_Play_pressed(view_idx):
-	
-	$Camera2D.smoothing_enabled = false
-	$Camera2D.position += $Camera2D.offset
-	
-	
 	$Camera2D.position = Vector2()
-	
-	
 	view_idx = min($VBoxContainer/HBoxContainer.get_child_count()-1, view_idx)
 	for i in range(view_idx, 0, -1):
 		$Camera2D.position.x += $VBoxContainer/HBoxContainer.get_child(i).rect_size.x
-	$Camera2D.offset = Vector2()
-	$Camera2D.smoothing_enabled = true
-
+		
+	currentPosX = $Camera2D.position.x
+	
 func _on_View_drag(event):
-	$Camera2D.offset.x -= event.relative.x
-
+	$Camera2D.position.x -= event.relative.x
 
 func _on_View_release(view):
-	
-	if $Camera2D.offset.x < -deadzone:
+	if $Camera2D.position.x-currentPosX < - deadzone:
 		_on_Play_pressed(view.get_index()-1)
-	elif $Camera2D.offset.x > deadzone:
+	elif $Camera2D.position.x-currentPosX > deadzone:
 		_on_Play_pressed(view.get_index()+1)
 	else:
 		_on_Play_pressed(view.get_index())
